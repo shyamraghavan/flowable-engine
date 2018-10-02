7cfab8ab411e06e37bdb72e0884499a696672a7a
Shyam Raghavan <shyamra@zillowgroup.com>
2018-10-02 14:34:03 -0700

    Add release scripts for 6.3.1 to 6.4.0 db upgrade

---
+++ distro/sql/upgrade/bpmn/flowable.mssql.upgradestep.6310.to.6400.engine.sql
@@ -0,0 +1,9 @@
+update ACT_GE_PROPERTY set VALUE_ = '6.4.0.0' where NAME_ = 'identitylink.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '6.4.0.0' where NAME_ = 'task.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '6.4.0.0' where NAME_ = 'variable.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '6.4.0.0' where NAME_ = 'job.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '6.4.0.0' where NAME_ = 'schema.version';
